ae7da2a2e7a8cd7fca6094654f1451cbf5030290
dev-gsilv <dev.gsilv@gmail.com>
2023-10-16 21:32:19 -0300

    feat: exercício da aula 04 do curso.

--- logica-de-programacao-1_aula-04/index.js
+++ logica-de-programacao-1_aula-04/index.js
@@ -1,31 +1,50 @@
 // TAREFA 01
-const pedidoOriginal = {
+const pedidoDinheiro = {
     nomeCliente: "Maria",
     nomeRestaurante: "Tacotitlan",
+    formaPagamento: "dinheiro",
     produtos: [
         {nomeProduto: "ceviche", valorUnitario: "39.90", quantidade: "4"},
         {nomeProduto: "taco al pastor", valorUnitario: "12.90", quantidade: "2"}
     ]
 }
 
-// TAREFA 02
-function calcularTotalPedido(pedido){
+function calcularTotalComDesconto(pedido){
     let somaTotal = 0;
     pedido.produtos.forEach(p => {
         somaTotal += p.valorUnitario * p.quantidade;
     });
+
+    switch(pedido.formaPagamento){
+        case "pix":
+            somaTotal *= 0.8;
+            break;
+        case "dinheiro":
+            somaTotal *= 0.85;
+            break;
+        case "cartaoDebito":
+            somaTotal *= 0.95;
+            break;
+        case "cartaoCredito":
+            somaTotal *= 0.99;
+            break;
+        default:
+            break;
+    }
+
     pedido.subtotal = somaTotal;
     return pedido;
 }
 
-let resultado = calcularTotalPedido(pedidoOriginal);
-console.log("Tarefa #2, subtotal do pedido",resultado);
+let resultado = calcularTotalComDesconto(pedidoDinheiro);
+console.log("Tarefa #1, subtotal do pedido com desconto",resultado,"\n## ## ## ##");
 
-// TAREFA 03:
+// TAREFA 02:
 let pedidosDoDia = [
     {
         nomeCliente: "Maria",
         nomeRestaurante: "Tacotitlan",
+        formaPagamento: "dinheiro",
         produtos: [
             {nomeProduto: "ceviche",valorUnitario: "39.90",quantidade: "4"},
             {nomeProduto: "taco al pastor",valorUnitario: "12.90",quantidade: "2"}
@@ -34,6 +53,7 @@ let pedidosDoDia = [
     {
         nomeCliente: "João",
         nomeRestaurante: "Tacotitlan",
+        formaPagamento: "pix",
         produtos: [
             {nomeProduto: "ceviche",valorUnitario: "39.90",quantidade: "2"},
             {nomeProduto: "taco al pastor",valorUnitario: "12.90",quantidade: "4"}
@@ -42,8 +62,18 @@ let pedidosDoDia = [
     {
         nomeCliente: "Rafa",
         nomeRestaurante: "Tacotitlan",
+        formaPagamento: "cartaoDebito",
         produtos: [
             {nomeProduto: "ceviche",valorUnitario: "39.90",quantidade: "1"},
+            {nomeProduto: "taco al pastor",valorUnitario: "12.90",quantidade: "1"}
+        ]
+    },
+    {
+        nomeCliente: "Fernanda",
+        nomeRestaurante: "Tacotitlan",
+        formaPagamento: "cartaoCredito",
+        produtos: [
+            {nomeProduto: "ceviche",valorUnitario: "39.90",quantidade: "5"},
             {nomeProduto: "taco al pastor",valorUnitario: "12.90",quantidade: "1"}
         ]
     }
@@ -52,7 +82,7 @@ let pedidosDoDia = [
 function calcularTotalDiario(pedidosArray){
     let totalDiario = 0;
     pedidosArray.forEach(p => {     
-        calcularTotalPedido(p);   
+        calcularTotalComDesconto(p);   
         totalDiario += p.subtotal;
         console.log(p)
     })
@@ -60,4 +90,4 @@ function calcularTotalDiario(pedidosArray){
 }
 
 resultado = calcularTotalDiario(pedidosDoDia).toFixed(2);
-console.log("Tarefa #3, total geral diário R$",resultado);
+console.log("Tarefa #2, total geral diário R$",resultado);
