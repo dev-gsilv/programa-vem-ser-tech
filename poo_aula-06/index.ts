aea71cf15cdacfbfa74412d7ef8a3c4bf869a8c0
dev-gsilv <dev.gsilv@gmail.com>
2023-11-17 14:48:14 -0300

    Implementada lógica para validar usuários repetidos

--- poo_aula-06/index.ts
+++ poo_aula-06/index.ts
@@ -7,48 +7,55 @@ interface Usuario {
 }
 
 function lerArquivo(caminho: string) {
-    let arquivoOriginal: string = fs.readFileSync(caminho, 'utf-8');
-    const _arquivoOriginal: [] = JSON.parse(arquivoOriginal);
-    return _arquivoOriginal;
+    let arquivoString: string = fs.readFileSync(caminho, 'utf-8');
+    const arquivoArray: Usuario[] = JSON.parse(arquivoString);
+    return arquivoArray;
 }
 
 function classificarUsuariosPorEstado(caminho: string) {
-    const usuarios: Usuario[] = lerArquivo(caminho);
+    const usuarios = lerArquivo(caminho);
 
     usuarios.forEach(u => {
+        console.log('Lendo usuario:', u.nome);
+
+        // Verifica se não existe o arquivo JSON do estado desse usuário. Ler um arquivo inexistente gera um erro
         if (!fs.existsSync(`./usuarios-por-estado/usuarios-${u.estado}.json`)) {
             fs.writeFileSync(
                 `./usuarios-por-estado/usuarios-${u.estado}.json`,
                 '[]',
             );
         }
-        const usuariosPorEstado = fs.readFileSync(
+        // Lê os dados do arquivo JSON
+        const arquivoPorEstadoString = fs.readFileSync(
             `./usuarios-por-estado/usuarios-${u.estado}.json`,
             'utf-8',
         );
-        const _usuariosPorEstado: Set<Usuario> = JSON.parse(usuariosPorEstado);
-        console.log(_usuariosPorEstado.size);
+        // Converte arquivoPorEstadoString em um array de usuarios
+        const usuarioPorEstadoArray: Usuario[] = JSON.parse(
+            arquivoPorEstadoString,
+        );
 
-        _usuariosPorEstado.forEach(p => {
-            console.log('usuarios por estado');
-
-            if (p.cpf == u.cpf ) {
-                console.log('cpf igual! ', p.cpf, u.cpf);
-                return;
-            } else if ( _usuariosPorEstado.size == 0 ) {
-                console.log('array vazio ');
-                
-                console.log('push ', p.cpf, u.cpf);
-
-                _usuariosPorEstado.add(u);
-                const _update = JSON.stringify(_usuariosPorEstado);
-
-                fs.writeFileSync(
-                    `./usuarios-por-estado/usuarios-${u.estado}.json`,
-                    _update,
-                );
-            }
+        // Validação para evitar usuarios duplicados
+        const usuariosCpfArray: string[] = [];
+        usuarioPorEstadoArray.forEach(pessoa => {
+            usuariosCpfArray.push(pessoa.cpf);
         });
+
+        if (usuariosCpfArray.includes(u.cpf)) {
+            return;
+        }
+
+        // Adiciona o usuario atual ao array de usuarioPorEstadoArray
+        usuarioPorEstadoArray.push(u);
+
+        // Transforma o array em string e salva no arquivo json
+        const _update = JSON.stringify(usuarioPorEstadoArray);
+
+        fs.writeFileSync(
+            `./usuarios-por-estado/usuarios-${u.estado}.json`,
+            _update,
+        );
+        console.log('Adicionado ao arquivo usuarios-'+u.estado+'.json');
     });
 }
 
