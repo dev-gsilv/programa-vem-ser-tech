23fa6c2e8ee08c419ddf64f29eae7f7759c7dbd4
dev-gsilv <dev.gsilv@gmail.com>
2024-01-10 20:33:43 -0300

    Aula 2 do módulo programação web interm.: REGEX.

--- node_express-01/src/server.js
+++ node_express-01/src/server.js
@@ -16,6 +16,26 @@ server.get('/lojas', (req, res) => {
     console.log(lojas.length, ' lojas cadastradas.');
     return res.status(200).json(lojas);
 });
+server.delete(
+    `/lojas/:tipo(restaurante|mercado|distribuidora)/:id`,
+    (req, res) => {
+        const params = req.params;
+        if (
+            lojas.some(loja => loja.id == params.id && loja.tipo == params.tipo)
+        ) {
+            lojas.splice(
+                lojas.findIndex(
+                    loja => loja.id == params.id && loja.tipo == params.tipo,
+                ),
+                1,
+            );
+            console.log('Loja removida. ID:', params.id);
+            return res.status(204).send();
+        }
+        console.log('Nenhuma loja removida com o ID:', params.id);
+        return res.status(404).send('msg: Nenhuma loja removida.');
+    },
+);
 
 const PORT = 3000;
 server.listen(PORT, () => {
