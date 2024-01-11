e9fd68a8dcfea7c8ab7bae38474a68d11b25c7ad
dev-gsilv <dev.gsilv@gmail.com>
2024-01-10 21:48:14 -0300

    Aula 2 do módulo programação web interm.: múltiplos handlers.

--- node_express-01/src/server.js
+++ node_express-01/src/server.js
@@ -5,14 +5,31 @@ server.use(express.json());
 let lojas = [];
 let idNovo = 0;
 
-server.post('/lojas', (req, res) => {
-    const loja = req.body;
-    loja.id = idNovo++;
-    lojas.push(loja);
-    console.log('Loja criada com sucesso. ID:', loja.id);
-    return res.status(201).json({ id: loja.id });
-});
-server.get('/lojas', (req, res) => {
+server.post(
+    '/lojas',
+    (req, res, next) => {
+        console.log('POST(/lojas) recebido', new Date());
+        next();
+    },
+    (req, res, next) => {
+        if (req.body && Object.keys(req.body).length > 0) {
+            next();
+        } else {
+            return res.status(400).send({ erro: 'req.body inválido!' });
+        }
+    },
+    (req, res, next) => {
+        const loja = req.body;
+        loja.id = idNovo++;
+        lojas.push(loja);
+        res.status(201).send({ id: loja.id });
+        next();
+    },
+    (req, res, next) => {
+        console.log('Loja criada com sucesso.', new Date());
+    },
+);
+server.get('/lojas', (req, res, next) => {
     console.log(lojas.length, ' lojas cadastradas.');
     return res.status(200).json(lojas);
 });
